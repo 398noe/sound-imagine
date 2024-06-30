2a892204c6cc6a30457b5902c31a419172aef4d7
SakyubaNoe <398noe@gmail.com>
2024-06-30 23:03:38 +0900

    Add imager cpp and imager h for handling fft result data

--- Source/component/Imager.cpp
+++ Source/component/Imager.cpp
@@ -1,1 +1,38 @@
 #include "component/Imager.h"
+
+Imager::Imager(std::shared_ptr<SharedAudioData> data) : shared_audio_data(data)
+{
+    // shared_audio_data = std::make_shared<SharedAudioData>();
+    startTimerHz(60);
+}
+
+Imager::~Imager()
+{
+    stopTimer();
+}
+
+void Imager::paint(juce::Graphics &g)
+{
+    g.fillAll(juce::Colours::black);
+    g.setColour(juce::Colours::white);
+    g.drawRect(getLocalBounds(), 1);
+}
+
+void Imager::resized()
+{
+}
+
+void Imager::timerCallback()
+{
+    drawFFTData();
+}
+
+void Imager::drawFFTData()
+{
+    if (shared_audio_data->is_next_fft_block_ready)
+    {
+        float *fft_data = shared_audio_data->getFFTData();
+        // shared_audio_data->is_next_fft_block_ready = false;
+    }
+}
+
--- Source/include/component/Imager.h
+++ Source/include/component/Imager.h
@@ -1,3 +1,24 @@
 #pragma once
 
 #include "lib/AudioUtilities.h"
+#include "lib/SharedAudioData.h"
+
+class Imager : public juce::Component, private juce::Timer
+{
+public:
+    Imager(std::shared_ptr<SharedAudioData> data);
+    ~Imager() override;
+
+    void paint(juce::Graphics &g) override;
+    void resized() override;
+
+    void timerCallback() override;
+
+    void drawFFTData();
+
+private:
+    bool is_next_fft_block_drawable = true;
+    std::shared_ptr<SharedAudioData> shared_audio_data;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Imager)
+};

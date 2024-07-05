87a4533d701265d60835b38c758998287ad67e2a
SakyubaNoe <398noe@gmail.com>
2024-07-05 12:46:22 +0900

    implement functions

--- Source/lib/Processor.cpp
+++ Source/lib/Processor.cpp
@@ -8,10 +8,32 @@ Processor::~Processor()
 {
 }
 
-float* Processor::getAudioSample(uint16_t offset)
+void Processor::doFFT()
 {
-    return &buffer[(buffer_idx + offset) & SharedAudioData::FFT_MASK];
+    float* block = getAudioSample(buffer_idx);
+    forwardFFT.performRealOnlyForwardTransform(block);
+    // block means fft result data
+}
 
+float* Processor::cloneAudioSample() {
+    float block[SharedAudioData::FFT_SIZE];
+    juce::zeromem(block, sizeof(block));
+    for (int i = 0; i < SharedAudioData::FFT_SIZE; i++)
+    {
+        block[i] = buffer[i];
+    }
+    return block;
+}
+
+float* Processor::getAudioSample(uint16_t start = 0)
+{
+    float block[SharedAudioData::FFT_SIZE];
+    juce::zeromem(block, sizeof(buffer));
+    for (int i = 0; i < SharedAudioData::FFT_SIZE; i++)
+    {
+        block[i] = buffer[(start + i) & SharedAudioData::FFT_MASK];
+    }
+    return block;
 }
 
 void Processor::addAudioSample(float sample)

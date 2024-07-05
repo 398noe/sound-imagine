9eb153829ba1d1fdba23251b6a404faf666d0e4b
SakyubaNoe <398noe@gmail.com>
2024-07-05 12:46:02 +0900

    Define manager and processor class

--- Source/include/lib/Manager.h
+++ Source/include/lib/Manager.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include "processor.h"
+
 class Manager {
 public:
+    static const int FFT_ORDER = 10;
+    static constexpr int FFT_SIZE = 1 << FFT_ORDER;
+    static constexpr int FFT_MASK = FFT_SIZE - 1;
+
     Manager();
     ~Manager();
+
+    void addAudioSample(float sample, int channel);
+    void calculateFFT();
+
+    void inferLR();
+
+private:
+    Processor processor[2]; // 0 is for left channel, 1 is for right
 };
--- Source/include/lib/Processor.h
+++ Source/include/lib/Processor.h
@@ -12,8 +12,9 @@ public:
 
     void doFFT();
 
-    float* getAudioSample(uint16_t offset);
-    void addAudioSample(float sample);
+    float* cloneAudioSample(); // clone buffer
+    float* getAudioSample(uint16_t start); // get buffer in chronological order
+    void addAudioSample(float sample); // add sample to buffer
 
 private:
     juce::dsp::FFT forwardFFT;

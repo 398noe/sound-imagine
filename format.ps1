fc8941f1b67a1bd56a01f4ce811bbca843fb81dc
SakyubaNoe <398noe@gmail.com>
2024-07-05 15:42:31 +0900

    make format script

--- format.ps1
+++ format.ps1
@@ -1,3 +1,3 @@
 echo "formatting..."
-clang-format -i -style=file Source/*.cpp Source/*.h
+clang-format -style=file:.clang-format -i $(Get-ChildItem -Path $PWD/Source -Recurse | Where Name -Match '.(?:cpp|h)$' | Select-Object -ExpandProperty FullName)
 echo "-> done!"

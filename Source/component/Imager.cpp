39c9c790071dc65a9f03b76947e82184dad827fc
SakyubaNoe <398noe@gmail.com>
2024-07-06 00:01:47 +0900

    fix size and type conversion

--- Source/component/Imager.cpp
+++ Source/component/Imager.cpp
@@ -7,24 +7,17 @@ Imager::~Imager() { stopTimer(); }
 void Imager::paint(juce::Graphics &g) {
     g.fillAll(juce::Colours::black);
 
-    std::cout << "paint" << std::endl;
-    // const int grid_size = 20;
     const int grid_size = 20;
-    // const int num_squares = fft_data[0].size();
-    // g.setColour(juce::Colours::white);
-    // g.drawText("Imager", 0, 0, getWidth(), 20, juce::Justification::centred);
-    // g.drawText("num squares: " + std::to_string(num_squares), 0, 20, getWidth(), 20, juce::Justification::centred);
-    // const int square_size = getWidth() / grid_size;
+    const int square_size = getWidth() / grid_size;
+    for (int i = 0; i < FFTConstants::FFT_LENGTH; i++) {
+        float value = fft_data[0][i];
+        juce::Colour colour = juce::Colour::fromHSV(0.5f + 0.5f * value, 1.0f, 1.0f, 1.0f);
+        int x = (i % grid_size) * square_size;
+        int y = (i / grid_size) * square_size;
 
-    // for (int i = 0; i < num_squares; ++i) {
-    //     float value = fft_data[0][i];
-    //     juce::Colour colour = juce::Colour::fromHSV(0.5f + 0.5f * value, 1.0f, 1.0f, 1.0f);
-    //     int x = (i % grid_size) * square_size;
-    //     int y = (i / grid_size) * square_size;
-
-    //     g.setColour(colour);
-    //     g.fillRect(x, y, square_size, square_size);
-    // }
+        g.setColour(colour);
+        g.fillRect(x, y, square_size, square_size);
+    }
 }
 
 void Imager::resized() { repaint(); }
@@ -38,8 +31,4 @@ void Imager::timerCallback() {
     }
 }
 
-void Imager::drawFFTData() {
-    // LFFT, RFFTからデシベルを計算
-    // fft_data[0] = manager->getFFTResult(0);
-    // fft_data[1] = manager->getFFTResult(1);
-}
+void Imager::drawFFTData() { fft_data = manager->getFFTResult(); }

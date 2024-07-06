f6a01cb3c10aa3bc8510691cb3da49bc83a8ba37
SakyubaNoe <398noe@gmail.com>
2024-07-06 18:30:16 +0900

    feat: Improve LRImager paint function

--- Source/component/LRImager.cpp
+++ Source/component/LRImager.cpp
@@ -5,25 +5,39 @@ LRImager::LRImager(std::shared_ptr<Manager> data) : manager(data) { startTimerHz
 LRImager::~LRImager() { stopTimer(); }
 
 void LRImager::paint(juce::Graphics &g) {
+    if (fft_data.empty() || power_spectrum.empty() || energy_difference.empty()) {
+        return;
+    }
     auto fft_freqs = manager->getFFTFreqs();
     fft_freqs[0] = 10.0f;
     auto width = getWidth();
     auto height = getHeight();
-    g.fillAll(juce::Colours::black);
+    auto center_x = width / 2.0f;
 
+    g.fillAll(juce::Colours::black);
     // LRのPower Spectrumを描画
-    g.setColour(juce::Colours::green);
     for (size_t i = 0; i < FFTConstants::FFT_LENGTH; i++) {
         auto freq = fft_freqs[i];
         auto log = std::log10(freq);
-        auto x = juce::jmap(log, 1.0f, 5.0f, 1.0f, 5.0f);
-        // 左は20Hz, 右は20kHzになるように描画
-        auto dot_x = juce::jmap(x, 1.0f, 5.0f, 0.0f, (float)width);
         auto left = power_spectrum[0][i];
         auto right = power_spectrum[1][i];
-        auto left_y = juce::jmap(left, 0.0f, 100.0f, (float)height, 0.0f);
-        auto right_y = juce::jmap(right, 0.0f, 100.0f, (float)height, 0.0f);
-        g.drawLine(dot_x, left_y, dot_x, right_y, 2.0f);
+        auto diff = energy_difference[0][i];
+        auto left_x = juce::jmap(left, 0.0f, 100.0f, center_x, 0.0f);
+        auto right_x = juce::jmap(right, 0.0f, 100.0f, center_x, float(width));
+        // 一番下が20Hz, 一番上が20kHzになるようにyの座標を決める。
+        // fft_freqsがそれ以外の範囲を持つ場合があるが, その場合はウィンドウの外に描画する
+        auto y = height - (log - 1.30103f) / 3.0f * height;
+        g.setColour(juce::Colours::green);
+        g.drawLine(left_x, y, right_x, y, 2.0f);
+
+        auto power = (left + right) * 0.5f;
+        auto color = juce::Colour::fromFloatRGBA(1.0f, 0.0f, 0.0f, juce::jmap(power, 0.0f, 10.0f, 0.5f, 1.0f));
+        auto offset = juce::jmap(diff, -50.0f, 50.0f, -0.5f, 0.5f);
+        auto dot_x = (1 + 2.0f * offset) * center_x;
+        g.setColour(color);
+        g.fillEllipse(dot_x, y, 4.0f, 4.0f);
+
+        
     }
 };
 
